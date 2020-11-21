d79bdd84cc8d41059a7ebb703b65a1f2ebe5b0b4
dina <nadhirahdina@gmail.com>
2020-11-21 17:43:04 +0700

    Create cleaning_data.py

---
+++ Deployment Model/cleaning_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+def data_case():
+    df = pd.read_csv('clean_data2.csv')
+    #df.drop('Unnamed : 0', axis=1, inplace=True)
+    return df
+
+
+
+
+
